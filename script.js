a8ea3ad1a1e45e50954a3cc39c2cc0e88589b9ab
Mario Valverde Rivera <rival.moro@hotmail.it>
2023-01-09 19:59:48 +0000

    adding score for player1 and dice manipulaton

--- script.js
+++ script.js
@@ -3,16 +3,35 @@
 // Selecting Elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
-const buttonsEl = document.querySelectorAll('.btn');
-const buttonRol = document.querySelector('.btn--roll');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 // Starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
-buttonRol.addEventListener('click', function () {
-  let diceNumber = Math.trunc(Math.random() * 6) + 1;
-  console.log(diceNumber);
+// Rolling dice functionality
+
+let currentScore = 0;
+
+btnRoll.addEventListener('click', function () {
+  // 1. Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 4) + 1;
+  // 2. Display dice number
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  // 3. check for rolled 1: if is true, switch to next player
+  if (dice !== 1) {
+    currentScore += dice;
+  } else {
+    currentScore = 0;
+  }
+  current0El.textContent = currentScore; // IMPORTANT change later
+  console.log(currentScore);
 });
