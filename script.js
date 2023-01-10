c5d1d2e458df76f856184f6c77e463664cedba0e
Mario Valverde Rivera <rival.moro@hotmail.it>
2023-01-10 19:19:37 +0000

    adding switch the active player moving the elements and values to the current player

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 'use strict';
 
 // Selecting Elements
-const score0El = document.querySelector('#score--0');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
@@ -15,13 +17,14 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
-// Rolling dice functionality
-
+const score = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
+// Rolling dice functionality
 btnRoll.addEventListener('click', function () {
   // 1. Generating a random dice roll
-  const dice = Math.trunc(Math.random() * 4) + 1;
+  const dice = Math.trunc(Math.random() * 6) + 1;
   // 2. Display dice number
   diceEl.classList.remove('hidden');
   diceEl.src = `dice-${dice}.png`;
@@ -29,9 +32,24 @@ btnRoll.addEventListener('click', function () {
   // 3. check for rolled 1: if is true, switch to next player
   if (dice !== 1) {
     currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
+    // Switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
     currentScore = 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+    
+    activePlayer = activePlayer === 0 ? 1 : 0;
+   
   }
-  current0El.textContent = currentScore; // IMPORTANT change later
-  console.log(currentScore);
 });
+
+// btnHold.addEventListener('click', function(){
+//   currentScore = 0;
+//   current0El.textContent = 0;
+
+//   // score0El = currentScore;
+//   // score0El.textContent = currentScore
+// })
